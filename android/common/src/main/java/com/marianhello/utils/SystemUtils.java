d6150b378fe15f7c574fb35414e729a886d94d63
rvgroup <rudenkovitalij@gmail.com>
2024-11-28 20:25:41 +0300

    fix Targeting U+ (version 34 and above) disallows creating or retrieving a PendingIntent with FLAG_MUTABLE

--- android/common/src/main/java/com/marianhello/utils/SystemUtils.java
+++ android/common/src/main/java/com/marianhello/utils/SystemUtils.java
@@ -13,7 +13,9 @@ public class SystemUtils {
     }
 
     public static int getMuttableIntentFlag(int intent) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            return intent | PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE | PendingIntent.FLAG_ALLOW_UNSAFE_IMPLICIT_INTENT;
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             return intent | PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE;
         } else {
             return intent;
